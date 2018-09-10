85c8ff14f08a0fbb75c26b78020c3bdaee58e8ff
Matt Conway <mattconway55@gmail.com>
2018-09-10 06:56:11 -0400

    Updated client-side implementation

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,20 @@
 const getData = async () => {
-  let response = await fetch('http://127.0.0.1:3001/api/v1/internship-search')
+  return await fetch('http://127.0.0.1:3001/api/v1/internship-search')
     .then(res => {
       return res.json();
     }).then(data => {
       return data;
   });
-  return response;
 }
+
+let sheetPromise = getData();
+
+const renderData = dataPromise => {
+  dataPromise.then(data => {
+    data.map(val => {
+      console.info(val);
+    })
+  })
+}
+
+renderData(sheetPromise);
